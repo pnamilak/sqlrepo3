e6be91bc4d92e6b1d7c852814a6ccb9fedf9e4c6
pnamilak <pranay.sqldba@gmail.com>
2025-06-18 17:27:00 +0530

    update

--- scripts/create_table_3.sql
+++ scripts/create_table_3.sql
@@ -1,1 +1,9 @@
--- Test update Wed Jun 18 08:47:02 IST 2025
+USE [TennisApp]
+GO
+
+/****** Object:  Table [dbo].[ServerDetailedInventory]    Script Date: 18-06-2025 17:24:55 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ServerDetailedInventory]') AND type in (N'U'))
+DROP TABLE [dbo].[ServerDetailedInventory]
+GO
+
+
